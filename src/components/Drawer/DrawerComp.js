ea7b2a48c17fd94f66129cf8b8bcc72e7edb5f59
Dominic Swaby <domswaby@gmail.com>
2023-09-27 15:57:53 -0400

    make navbar responsive using drawer and icon button

---
+++ src/components/Drawer/DrawerComp.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+
+import {
+  Drawer,
+  IconButton,
+  List,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+} from "@mui/material";
+import MenuRoundedIcon from "@mui/icons-material/MenuRounded";
+
+const DrawerComp = () => {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <>
+      <Drawer open={open} onClose={() => setOpen(false)}>
+        <List>
+          <ListItemButton>
+            <ListItemIcon>
+              <ListItemText>Products</ListItemText>
+            </ListItemIcon>
+          </ListItemButton>
+        </List>
+      </Drawer>
+      <IconButton
+        sx={{ marginLeft: "auto", color: "white" }}
+        onClick={() => setOpen(!open)}
+      >
+        <MenuRoundedIcon />
+      </IconButton>
+    </>
+  );
+};
+
+export default DrawerComp;
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -9,45 +9,67 @@ import {
   Tabs,
   Toolbar,
   Typography,
+  useTheme,
+  useMediaQuery,
 } from "@mui/material";
 import StoreIcon from "@mui/icons-material/Store";
+import DrawerComp from "../Drawer/DrawerComp";
 
 const NavBar = ({ links }) => {
+  const theme = useTheme();
+  const isMatch = useMediaQuery(theme.breakpoints.down("sm"));
   const [value, setValue] = useState();
 
   return (
-    <AppBar>
+    <AppBar
+      sx={{
+        backgroundImage:
+          "linear-gradient(90deg, rgba(180,58,58,1) 2%, rgba(49,49,116,1) 36%, rgba(105,0,161,1) 73%, rgba(166,69,252,1) 100%)",
+      }}
+    >
       <Toolbar>
-        <Grid container>
-          <Grid item xs={2}>
-            <Typography>
-              <StoreIcon />
-            </Typography>
+        {isMatch ? (
+          <>
+            <DrawerComp />
+          </>
+        ) : (
+          <Grid sx={{ placeItems: "center" }} container>
+            <Grid item xs={2}>
+              <Typography>
+                <StoreIcon />
+              </Typography>
+            </Grid>
+            <Grid xs={6}>
+              <Tabs
+                indicatorColor="secondary"
+                textColor="inherit"
+                value={value}
+                onChange={(e, val) => setValue(val)}
+              >
+                {links.map((link, index) => (
+                  <Tab key={index} label={link} />
+                ))}
+              </Tabs>
+            </Grid>
+            <Grid item xs={1}></Grid>
+            <Grid item xs={3}>
+              <Box display="flex">
+                <Button
+                  sx={{ marginLeft: "auto", background: "rgba(180,58,58,1)" }}
+                  variant="contained"
+                >
+                  Login
+                </Button>
+                <Button
+                  sx={{ marginLeft: 1, background: "rgba(180,58,58,1)" }}
+                  variant="contained"
+                >
+                  Signup
+                </Button>
+              </Box>
+            </Grid>
           </Grid>
-          <Grid xs={6}>
-            <Tabs
-              indicatorColor="secondary"
-              textColor="inherit"
-              value={value}
-              onChange={(e, val) => setValue(val)}
-            >
-              {links.map((link, index) => (
-                <Tab key={index} label={link} />
-              ))}
-            </Tabs>
-          </Grid>
-          <Grid item xs={1}></Grid>
-          <Grid item xs={3}>
-            <Box display="flex">
-              <Button sx={{ marginLeft: "auto" }} variant="contained">
-                Login
-              </Button>
-              <Button sx={{ marginLeft: 1 }} variant="contained">
-                Signup
-              </Button>
-            </Box>
-          </Grid>
-        </Grid>
+        )}
       </Toolbar>
     </AppBar>
   );
