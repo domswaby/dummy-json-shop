cb899051b60b6e5ca721ffa2a6eb8be0b9045a8c
Dominic Swaby <domswaby@gmail.com>
2023-09-28 20:06:39 -0400

    import and style paginator

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -2,13 +2,17 @@ import React from "react";
 import "./Shop.css";
 import Search from "../Search/Search";
 import Gallery from "../Gallery/Gallery";
+import Pagination from "@mui/material/Pagination";
+import { Box, Stack } from "@mui/material";
 
 const Shop = () => {
   return (
     <div className="shop-container">
-      
       {/* <Search /> */}
       <Gallery />
+      <Stack alignItems="center" className="pagination-stack">
+        <Pagination count={10} color="secondary" size="large" />
+      </Stack>
     </div>
   );
 };
